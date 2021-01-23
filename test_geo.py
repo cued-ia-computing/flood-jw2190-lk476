38a7e32094a77be0a6b43d683b8dbf92ec4822fb
Jordan <jw2190@cam.ac.uk>
2021-01-23 23:55:45 +0000

    Added test_stations_within_radius function

--- test_geo.py
+++ test_geo.py
@@ -2,7 +2,8 @@
 from floodsystem.station import MonitoringStation
 from haversine import haversine
 from floodsystem.utils import sorted_by_key
-
+from floodsystem.geo import stations_within_radius
+from floodsystem.stationdata import build_station_list
 def test_stations_by_distance():
     p = (0,0)
     #create 3 test stations:
@@ -21,3 +22,13 @@ def test_stations_by_distance():
     assert finaldata[0][0] == "closest"
     assert finaldata[1][0] == "middle"
     assert finaldata[2][0] == "furthest"
+
+def test_stations_within_radius():
+    """check that the correct stations are found when
+    looking at a 10km radius around Cam City Centre"""
+    CamCityCentre = (52.2053, 0.1218)
+    stations = build_station_list(use_cache=True)
+    stationnames = []
+    for station in stations_within_radius(stations, CamCityCentre, 10):
+        stationnames.append(station.name)
+    assert 'Bin Brook' in stationnames or 'Comberton' in stationnames or 'Oakington' in stationnames
