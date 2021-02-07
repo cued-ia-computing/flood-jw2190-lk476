8f8f64b4b73e0600bdd78e6e77950b90def6c961
Jordan <jw2190@cam.ac.uk>
2021-02-07 14:56:21 +0000

    Fixed rivers_by_station_number function

--- Task1E.py
+++ Task1E.py
@@ -3,18 +3,17 @@
 from floodsystem.geo import rivers_by_station_number
 from floodsystem.stationdata import build_station_list
 
+
 def run():
     """requirements for Task 1E"""
-    
-    #build list of stations
+
+    # build list of stations
     stations = build_station_list()
-    
-    #print the 9 rivers that have the most stations and how many stations are on each in the form of a list of tuples
-    rivers_station = rivers_by_station_number(stations, N = 10)
-    for i in rivers_station:
-        if rivers_station[0][i] == rivers_station[0][i+1]:
-            N += 1
+
+    # print the 9 rivers that have the most stations and how many stations are on each in the form of a list of tuples
+    rivers_station = rivers_by_station_number(stations, N=9)
     print("9 rivers with the most stations are: {}".format(rivers_station))
+
 
 if __name__ == "__main__":
     print("*** Task 1E: CUED Part IA Flood Warning System ***")
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -34,8 +34,8 @@ def stations_within_radius(stations, centre, r):
 
 
 def rivers_with_station(stations):
-    """returns a list, from a given list of station objects, of river names with a monitoring station""" 
-    rivers = set() # build an empty set
+    """returns a list, from a given list of station objects, of river names with a monitoring station"""
+    rivers = set()  # build an empty set
     for station in stations:
         river = station.river
         rivers.add(river)
@@ -59,14 +59,18 @@ def station_by_river(stations):
 def rivers_by_station_number(stations, N):
     """Determines the N rivers with the greatest number 
     of monitoring stations in the form of a (river name, number of stations) tuple"""
-    rivers = {}
-    
-    for station in stations:
-        river = station.river
-        if river in rivers:
-            rivers[river] = rivers[river] + 1
-        else:
-            rivers[river] = 1
-
-    return sorted(rivers.items(),key = lambda x: x[1], reverse=True)[:N]
-        
+    rivers = station_by_river(stations)
+    rivers_tobecut = []
+    rivers_final = []
+    for river in rivers.keys():
+        # ceates the tuple with first term being river name and second being number of stations for the river:
+        rivers_tobecut.append((river, len(rivers[river])))
+    # sorts the massive list of (river, station number) tuples:
+    rivers_tobecut = sorted(rivers_tobecut, key=lambda num: num[1], reverse=True)
+    # finds the minimum number of stations allowed to enter final list:
+    minallowed = sorted([x[1] for x in rivers_tobecut], reverse=True)[N + 1]
+    # adds the tuples with large enough no. of rivers to final list:
+    for river in rivers_tobecut:
+        if river[1] > minallowed:
+            rivers_final.append(river)
+    return rivers_final
