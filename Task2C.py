a3d16914decb9259881c3e4882c36515b0676a99
Jordan <jw2190@cam.ac.uk>
2021-02-20 23:09:05 +0000

    completed 2C

--- Task2C.py
+++ Task2C.py
@@ -1,13 +1,23 @@
 from floodsystem.flood import stations_highest_rel_level
-from floodsystem.stationdata import build_station_list
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.station import consistent_typical_range_stations, remove_latest_level_nonetype
 
 def run():
     """Requirements for Task2C"""
 
     #build list of stations
     stations = build_station_list()
+    #update water levels
+    update_water_levels(stations)
+    #clean station data
+    stations = consistent_typical_range_stations(stations)
+    stations = remove_latest_level_nonetype(stations)
+    #get the 10 stations with the highest relative water level
+    highest_rel_stations = stations_highest_rel_level(stations, N=10)
+    for station in highest_rel_stations:
+        print(f'{station[0].name} {station[1]}')
 
-    #print the 10 stations with the highest relative water level
-    water_level = stations_highest_rel_level(stations, N=10)
-    print("10 stations with the highest relative water level: {}".format(water_level))
 
+if __name__ == "__main__":
+    print("*** Task 2C: CUED Part 1A Flood Warning System ***")
+    run()
--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -15,8 +15,18 @@ def stations_level_over_threshold(stations, tol):
 def stations_highest_rel_level(stations, N):
     """returns a list of N stations at which the water level relative a typical range is highest.
     The list should be order in descending order by relative level."""
+    stations_tosend = []
+    relative_levels = []
+    for station in stations:
+        relative_levels.append((station,station.relative_water_level()))
+    relative_levels = sorted_by_key(relative_levels,1,reverse=True)
+    for i in range(N):
+        stations_tosend.append(relative_levels[i])
+    return stations_tosend
+    '''
     water_levels = set()
     for station in stations:
         water_level = station.relative_water_level
         water_levels.add(water_level)
         return sorted(station.relative_water_level)
+    '''
