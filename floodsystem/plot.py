6ed5e1e26a1bfb2d7737621bb35e69248a4c5660
Jordan <jw2190@cam.ac.uk>
2021-02-21 21:06:05 +0000

    Small change made to 1E

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -1,15 +1,15 @@
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-def plot_water_levels(data):
+def plot_water_levels(station_levels_list):
     '''plots water level data and typical low and high for up to 6 different stations.
     must be fed a list of (station,dates,levels) tuples'''
     locations = [321,322,323,324,325,326]
-    for entry in data:
+    for entry in station_levels_list:
         station = entry[0]
         dates = entry[1]
         levels = entry[2]
-        plt.subplot(locations[data.index(entry)])
+        plt.subplot(locations[station_levels_list.index(entry)])
         typical_low = [station.typical_range[0]]*len(dates)
         typical_high = [station.typical_range[1]]*len(dates)
         plt.plot(dates,levels,label='Water Levels')
